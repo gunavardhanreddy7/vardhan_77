ec4a912b4d5f9ba1b8c2f88303a766a6c6c293a0
gunavardhanreddy7 <31956763+gunavardhanreddy7@users.noreply.github.com>
2017-09-14 20:47:52 +0530

    com4

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,13 @@
+
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args)  {
 		
 		//You may test that your code works find here
 		//Please check that your code works and has no 
 		//compilation problems before to submit
+		Student s = new Student(538,"Akhil Munagala",'1996-06-20',61.1);
+
 	}
 
 }
--- src/StudentGroup.java
+++ src/StudentGroup.java
@@ -11,10 +11,9 @@ import java.util.Date;
  * DO NOT PUT any classes into packages
  *
  */
- class StudentGroup implements StudentArrayOperation 
- {
-	 Student[] students;
- 
+public class StudentGroup implements StudentArrayOperation {
+
+	private Student[] students;
 	
 	/**
 	 * DO NOT remove or change this constructor, it will be used during task check
@@ -27,147 +26,79 @@ import java.util.Date;
 	@Override
 	public Student[] getStudents() {
 		// Add your implementation here
-		return this.students;
+		return null;
 	}
 
 	@Override
 	public void setStudents(Student[] students) {
 		// Add your implementation here
-		this.students=students;
 	}
 
 	@Override
 	public Student getStudent(int index) {
 		// Add your implementation here
-		return this.students[index];
+		return null;
 	}
 
 	@Override
 	public void setStudent(Student student, int index) {
 		// Add your implementation here
-		this.students[index]=student;
 	}
 
 	@Override
 	public void addFirst(Student student) {
 		// Add your implementation here
-		int i=this.students.length;
-		for(int j=i;j>0;j--)
-		this.students[j]=this.students[j-1];
-		this.students[0]=student;
-		this.students[i+1]=null;
 	}
 
 	@Override
 	public void addLast(Student student) {
 		// Add your implementation here
-		int i=this.students.length;
-		this.students[i]=student;
-		this.students[i+1]=null;	
 	}
 
 	@Override
 	public void add(Student student, int index) {
 		// Add your implementation here
-		int i=this.students.length;
-		for(int j=i;j>index;j--)
-		this.students[j]=this.students[j-1];
-		this.students[index]=student;
-		this.students[i+1]=null;
 	}
 
 	@Override
 	public void remove(int index) {
 		// Add your implementation here
-		int i=this.students.length;
-		for(int j= index;j<i;j++)
-		this.students[j]=this.students[j+1];
-		this.students[i+1]=null;
 	}
 
 	@Override
 	public void remove(Student student) {
 		// Add your implementation here
-		int i=this.students.length;
-		int index=0;
-             for(int j=0;j<i;j++)
-		if(this.students[j].equals(student))
-		{index=j;
-		break;
-                }
-		for(int j=index;j<i;j++)
-		this.students[j]=this.students[j+1];
-		this.students[i+1]=null;
 	}
 
 	@Override
 	public void removeFromIndex(int index) {
 		// Add your implementation here
-		int i=this.students.length;
-		
-		this.students[index]=null;
 	}
 
 	@Override
 	public void removeFromElement(Student student) {
-		
 		// Add your implementation here
-		int i=this.students.length;
-		int index=0;
-             for(int j=0;j<i;j++)
-		if((this.students[j]).equals(student))
-		{index=j;
-		break;
-                }
-	
-		this.students[index]=null;
 	}
 
 	@Override
 	public void removeToIndex(int index) {
 		// Add your implementation here
-		int j=this.students.length;
-		for(int i=0; i<= j-index;i++)
-		this.students[i]=this.students[i+index+1];
-		this.students[j+1]=null;
 	}
 
 	@Override
 	public void removeToElement(Student student) {
 		// Add your implementation here
-		int i=this.students.length;
-		int index=0;
-             for(int j=0;j<i;j++)
-		if((this.students[j]).equals(student))
-		{index=j;
-		break;
-                }
-		int j=this.students.length;
-		for(i=0;i<=j-index;i++)
-		this.students[i]=this.students[i+index+1];
-		this.students[i+1]=null;
 	}
 
 	@Override
 	public void bubbleSort() {
 		// Add your implementation here
-		int n=this.students.length;
-		for(int i=0;i<n-1;i++)
-			for(int j=0;j<n-i-1;j++)
-				if((this.students[j]).isGreaterThan(this.students[j+1]))
-				{
-                                   Student student= new Student();
-				    student=this.students[i];
-				    this.students[i]=this.students[j];
-		    		    this.students[j]=student;	
-                                 
 	}
 
 	@Override
 	public Student[] getByBirthDate(Date date) {
 		// Add your implementation here
-		
-		
+		return null;
 	}
 
 	@Override
@@ -191,40 +122,18 @@ import java.util.Date;
 	@Override
 	public Student[] getStudentsByAge(int age) {
 		// Add your implementation here
-	
+		return null;
 	}
 
 	@Override
 	public Student[] getStudentsWithMaxAvgMark() {
 		// Add your implementation here
-		int l=this.students.length;
-		int max=this.students[0].avgMark;
-		for(int i=0;i<l;i++)
-		 if(this.students[i].avgMark > max)
-			max=this.students[i].avgMark;
- 
-			Student students1[]= new Student();
-			int k=0;
-			for(int j=0;j<n;j++)
-			{
-                           if(this.students[i].avgMark == max)
-				students1[k++]=this.students[i];
-                         }
-			return students1;
-		
+		return null;
 	}
 
 	@Override
 	public Student getNextStudent(Student student) {
 		// Add your implementation here
-	    int i=this.students.length;
-		int index1=0;
-             for(int j=0;j<i;j++)
-		if((this.students[j]).equals(student))
-		{index1=j;
-		break;
-                }
-        	return this.students[index1+1];
-		
+		return null;
 	}
 }
